65aadfd7f36289a46c0dda506955b00b7ae2856b
Benjie Harthun <bharthun7@gmail.com>
2025-08-13 20:42:19 -0500

    Create function to handle entire game and move round function and score variables inside

--- game.js
+++ game.js
@@ -11,21 +11,23 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Pick rock, paper, or scissors: ");
 }
-let humanScore = 0;
-let computerScore = 0;
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
-  if (humanChoice === computerChoice) {
-    console.log(`It's a tie! Both picked ${humanChoice}!`);
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
-  ) {
-    console.log(`Player wins! ${humanChoice} beats ${computerChoice}!`);
-    humanScore++;
-  } else {
-    console.log(`Computer wins! ${computerChoice} beats ${humanChoice}!`);
-    computerScore++;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+      console.log(`It's a tie! Both picked ${humanChoice}!`);
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "scissors" && computerChoice === "paper") ||
+      (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+      console.log(`Player wins! ${humanChoice} beats ${computerChoice}!`);
+      humanScore++;
+    } else {
+      console.log(`Computer wins! ${computerChoice} beats ${humanChoice}!`);
+      computerScore++;
+    }
   }
 }
