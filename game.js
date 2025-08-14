1995ea1900150040a67e8b2058a3e981f41bb4e2
Benjie Harthun <bharthun7@gmail.com>
2025-08-13 20:35:09 -0500

    Create function containing logic for single round of play

--- game.js
+++ game.js
@@ -13,3 +13,19 @@ function getHumanChoice() {
 }
 let humanScore = 0;
 let computerScore = 0;
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log(`It's a tie! Both picked ${humanChoice}!`);
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
+  ) {
+    console.log(`Player wins! ${humanChoice} beats ${computerChoice}!`);
+    humanScore++;
+  } else {
+    console.log(`Computer wins! ${computerChoice} beats ${humanChoice}!`);
+    computerScore++;
+  }
+}
