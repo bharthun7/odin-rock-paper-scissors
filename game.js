ac97219a5ea19554af4aacafc8da166085661c20
Benjie Harthun <bharthun7@gmail.com>
2025-08-16 22:04:06 -0500

    Update score on round completion

--- game.js
+++ game.js
@@ -9,62 +9,43 @@ function getComputerChoice() {
   }
 }
 function playRound(humanChoice, computerChoice) {
-  let msg=document.querySelector(".message");
+  let msg = document.querySelector(".message");
   if (humanChoice === computerChoice) {
-    msg.textContent=`It's a tie! Both picked ${humanChoice}!`;
+    msg.textContent = `It's a tie! Both picked ${humanChoice}!`;
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "paper") ||
     (humanChoice === "paper" && computerChoice === "rock")
   ) {
-    msg.textContent=
-      `Player wins! ${
-        humanChoice.at(0).toUpperCase() + humanChoice.slice(1)
-      } beats ${computerChoice}!`
-    ;
+    msg.textContent = `Player wins! ${
+      humanChoice.at(0).toUpperCase() + humanChoice.slice(1)
+    } beats ${computerChoice}!`;
     humanScore++;
   } else {
-    msg.textContent=
-      `Computer wins! ${
-        computerChoice.at(0).toUpperCase() + computerChoice.slice(1)
-      } beats ${humanChoice}!`
-    ;
+    msg.textContent = `Computer wins! ${
+      computerChoice.at(0).toUpperCase() + computerChoice.slice(1)
+    } beats ${humanChoice}!`;
     computerScore++;
   }
+  document.querySelector(".player").textContent = `Player Score: ${humanScore}`;
+  document.querySelector(
+    ".computer"
+  ).textContent = `Computer Score: ${computerScore}`;
+  document.querySelector(".ties").textContent = `Ties: ${ties}`;
+  /*
+  if(computerScore+humanScore+ties==5){
+    gameOver=true;
+    if 
+  }
+    */
 }
 let humanScore = 0;
 let computerScore = 0;
 let ties = 0;
 let btns = document.querySelector(".buttons");
+let gameOver = false;
 btns.addEventListener("click", (e) => {
-  playRound(e.target.className, getComputerChoice());
-});
-
-/*
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  playRound(getHumanChoice(), getComputerChoice());
-  if (humanScore === computerScore) {
-    console.log(
-      `It's a tie! Final scores:\n  Player: ${humanScore}\n  Computer: ${computerScore}\n  Ties: ${
-        5 - humanScore - computerScore
-      }`
-    );
-  } else if (humanScore > computerScore) {
-    console.log(
-      `Player wins! Final scores:\n  Player: ${humanScore}\n  Computer: ${computerScore}\n  Ties: ${
-        5 - humanScore - computerScore
-      }`
-    );
-  } else {
-    console.log(
-      `Computer wins! Final scores:\n  Player: ${humanScore}\n  Computer: ${computerScore}\n  Ties: ${
-        5 - humanScore - computerScore
-      }`
-    );
+  if (!gameOver) {
+    playRound(e.target.className, getComputerChoice());
   }
-  */
-
-//playGame();
+});
